24867cbe4cf1de60023b8a4cf80f0195eb7170c7
sunny504 <61198767+sunny504@users.noreply.github.com>
2021-04-26 17:29:17 +0530

    changes for creating user through post method

--- route.config.js
+++ route.config.js
@@ -1,10 +1,12 @@
-exports.routesConfig =  (app) => {
+const fs = require('fs');
 
+exports.routesConfig =  (app) => {
 
 
     app.get('/',async (req,res) => {
         try {
-    
+          var data = fs.readFileSync('userInfo.json');
+          let userList = JSON.parse(data);
           res.send({ statusCode : 200,
             message:"Successfully Found List.",
             data :[userList] });
@@ -14,5 +16,36 @@ exports.routesConfig =  (app) => {
             message:e.message});
         }
       });
+      
+      // post method
+      app.post('/addUser', async (req, res) => {
+
+      const getData = fs.readFileSync('userInfo.json')
+      const dataJSON = getData.toString()
+      const parseData = JSON.parse(dataJSON)
+
+         const userDetails={
+          name : req.body.name,
+          age  : req.body.age,
+          gender : req.body.gender,
+          department : req.body.department
+         }
+
+         let data =  parseData.push(userDetails)
+
+         const jsonData = JSON.stringify(data);
+
+         fs.writeFileSync('userInfo.json', jsonData)
+
+        try {
+          res.send({statusCode : 200,
+            message:"getting post request"
+        });
+        } catch (error) {
+          res.send({statusCode : 202,
+            message:e.message});
+        }
+        
+      })
 
 }
