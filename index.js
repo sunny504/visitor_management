d9d1a325b37e26a5a545a75fc1f73cbba75722b6
root <bmsunny04@gmail.com>
2021-04-26 17:24:53 +0530

    chages

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const userInfo = require('./route.config');
+
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+  next();
+});
+
+userInfo.routesConfig(app);
